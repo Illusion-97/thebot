133d6354e732fc36e2318bb12cabb2f7607a3dd4
Yanis ADEKALOM <yadekalom@gmail.com>
2024-12-13 14:38:16 +0100

    Feat! : Random Winner

--- src/main/java/dwn/cda/thebot/services/FightService.java
+++ src/main/java/dwn/cda/thebot/services/FightService.java
@@ -3,9 +3,18 @@ package dwn.cda.thebot.services;
 import lombok.NonNull;
 import net.dv8tion.jda.api.entities.User;
 
+import java.security.SecureRandom;
+
 public class FightService {
+
+    private static SecureRandom random = new SecureRandom();
+
     public static String requestFight(@NonNull User asker,@NonNull User target) {
-        return "%s challenges %s in an epic fight !%nWho'll come out with glory ?"
-                .formatted(asker.getEffectiveName(),target.getEffectiveName());
+        return "%s challenges %s in an epic fight !%nWho'll come out with glory ?%nAfter some marvelous strikes %s dominate the opponent and increase their fame ! HURRAY !"
+                .formatted(
+                        asker.getEffectiveName(),
+                        target.getEffectiveName(),
+                        (random.nextBoolean() ? asker : target).getEffectiveName()
+                );
     }
 }
